e18eef810b809811943978ab5ab3516515771206
Radek <radek511@op.pl>
2018-06-17 15:43:57 +0200

    Add - Vue Router
    Add 3 basic routes (home, shop, contact)

---
+++ src/Router/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/Components/Home.vue'
+import Shop from '@/Components/Shop.vue'
+import Contact from '@/Components/Contact.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/shop',
+      name: 'Shop',
+      component: Shop
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    }
+  ]
+})
