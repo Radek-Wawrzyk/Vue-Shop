2f77c9b79a14e81b5989499df0aeeadc33b9ba0f
Radek <radek511@op.pl>
2018-06-18 16:17:25 +0200

    Add - ResetCSS

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-5">5</div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 
+  @import "@/Styles/ResetCSS/reset.scss";
   @import "@/Styles/Grid/grid.scss";
 
 </style>
