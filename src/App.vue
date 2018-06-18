f1ce4dfa0f19ce0c6ecc7f4a4f9011a1a864d3fb
Radek <radek511@op.pl>
2018-06-18 17:18:32 +0200

    Add - Typography SCSS file
    Also one change in Reset CSS (font-size: 15px), and add presentation for typography in App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,15 @@
     </div>
     <div class="container">
       <div class="row">
-        <div class="col-5">5</div>
+        <div class="col-md-5">
+          <h1>Header H1</h1>
+          <h2>Header H2</h2>
+          <h3>Header H3</h3>
+          <h4>Header H4</h4>
+          <h5>Header H5</h5>
+          <p>Website in progress, god damm it, please fill this website! </p>
+          <a href="#">Link</a>
+        </div>
       </div>
     </div>
     <router-view/>
