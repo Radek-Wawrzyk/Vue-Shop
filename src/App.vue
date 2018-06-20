577a52cc18bf9d597895117a25ff5603fe53c789
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-20 18:02:35 +0200

    Add - Navigation bar with 4 menu items and empty sections for Brand and Hamburger Button

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-5">
-          <h1>Header H1</h1>
-          <h2>Header H2</h2>
-          <h3>Header H3</h3>
-          <h4>Header H4</h4>
-          <h5>Header H5</h5>
-          <p>Website in progress, god damm it, please fill this website! </p>
-          <a href="#">Link</a>
+    <div id="app">
+        <header>
+            <Navigation />
+        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <h1>Header H1</h1>
+                    <h2>Header H2</h2>
+                    <h3>Header H3</h3>
+                    <h4>Header H4</h4>
+                    <h5>Header H5</h5>
+                    <p>Website in progress, god damm it, please fill this website! </p>
+                    <a href="#">Link</a>
+                </div>
+            </div>
         </div>
-      </div>
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-
   @import "@/Styles/ResetCSS/reset.scss";
   @import "@/Styles/Grid/grid.scss";
   @import "@/Styles/main.scss";
-  
-
 </style>
+
+<script>
+    import Navigation from './Components/Navigation.vue';
+
+    export default
+        {
+            name: "app",
+            components: { Navigation }
+        }
+</script>
---
+++ src/Components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navigation-container">
+        <div class="navigation-brand">
+
+        </div>
+        <ul class="navigation-menu">
+            <li><a href="#">Home </a></li>
+            <li><a href="#">Shop </a></li>
+            <li><a href="#">About </a></li>
+            <li><a href="#">Contact </a></li>
+        </ul>
+        <div class="navigation-icons">
+
+        </div>
+    </nav>
+</template>
+
+<style lang="scss">
+    @import "@/Styles/Components/Navigation.scss";
+</style>
+
+<script>
+    export default {}
+</script>
