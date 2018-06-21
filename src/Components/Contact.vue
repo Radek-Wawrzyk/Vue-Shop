01fbc0e5e078fa129f1d1c069d1439d0f62b0762
Radek <radek511@op.pl>
2018-06-21 16:52:29 +0200

    Add - Map for Contact page
    Also add icons with information

--- src/Components/Contact.vue
+++ src/Components/Contact.vue
@@ -1,69 +1,112 @@
 <template>
-  <section id="contact">
-    <div class="contact-map">
-      There will be map with location
-    </div>
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-9">
-          <header class="contact-header">
-            <h1 class="contact-title">Contact us</h1>
-            <p class="contact-description">
-              Pathetic text about that we care about clients and their happines and other bulshit. 
-              Notice that it's fucking placeholder, so get the fuck out over here.
-            </p>
-          </header>
+  <main>
+    <section id="contact">
+      <div class="contact-map">
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2317.304043924749!2d18.53720831602352!3d54.49286009591487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fda0c8f6a33105%3A0xa2dd70483b763ed1!2sPPNT+Gdynia!5e0!3m2!1spl!2spl!4v1529589490438" 
+          width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen>
+        </iframe>
+      </div>
+      <div class="container">
+        <div class="contact-info">
+          <div class="row">
+            <div class="col-lg-3 col-md-6">
+              <div class="contact-tile">
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/></svg>
+                 <p class="contact-tile-text">
+                  Poland, Gdynia
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="contact-tile">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"/></svg>
+                <p class="contact-tile-text">
+                  +48 516 649 611
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="contact-tile">
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"/></svg>
+                 <p class="contact-tile-text">
+                   info@nasz-team.pl
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="contact-tile">
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"/></svg>
+                 <p class="contact-tile-text">
+                   www.nasz-team.pl
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <form class="contact-form">
-        <div class="row">
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="text" placeholder="Your Name" v-model="fields.name" v-bind:class="{'error': errors.name}">
-              <transition name="fade-form">
-                <p class="form-error" v-if="errors.name">{{errors.name}}</p>
-              </transition>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="email" placeholder="Your Email" v-model="fields.email" v-bind:class="{'error': errors.email}">
-              <transition name="fade-form">
-                <p class="form-error" v-if="errors.email">{{errors.email}}</p>
-              </transition>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="text" placeholder="Your Address" v-model="fields.address" v-bind:class="{'error': errors.address}">
-              <transition name="fade-form">
-                <p class="form-error" v-if="errors.address">{{errors.address}}</p>
-              </transition>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="text" placeholder="Your Phone" v-model="fields.phone" v-bind:class="{'error': errors.phone}">
-              <transition name="fade-form">
-                <p class="form-error" v-if="errors.phone">{{errors.phone}}</p>
-              </transition>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <div class="form-group">
-              <textarea placeholder="Your Message" rows="9" v-model="fields.message" v-bind:class="{'error': errors.message}"></textarea>
-              <transition name="fade-form">
-                <p class="form-error" v-if="errors.message">{{errors.message}}</p>
-              </transition>
-            </div>
-          </div>
-          <div class="col-md-12">
-            <button type="submit" @click="formValidate" >Send Message</button>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-9">
+            <header class="contact-header">
+              <h1 class="contact-title">Contact us</h1>
+              <p class="contact-description">
+                Pathetic text about that we care about clients and their happines and other bulshit. 
+                Notice that it's fucking placeholder, so get the fuck out over here.
+              </p>
+            </header>
           </div>
         </div>
-      </form>
-    </div>
-  </section>
+        <form class="contact-form">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="text" placeholder="Your Name" v-model="fields.name" v-bind:class="{'error': errors.name}">
+                <transition name="fade-form">
+                  <p class="form-error" v-if="errors.name">{{errors.name}}</p>
+                </transition>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="email" placeholder="Your Email" v-model="fields.email" v-bind:class="{'error': errors.email}">
+                <transition name="fade-form">
+                  <p class="form-error" v-if="errors.email">{{errors.email}}</p>
+                </transition>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="text" placeholder="Your Address" v-model="fields.address" v-bind:class="{'error': errors.address}">
+                <transition name="fade-form">
+                  <p class="form-error" v-if="errors.address">{{errors.address}}</p>
+                </transition>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="text" placeholder="Your Phone" v-model="fields.phone" v-bind:class="{'error': errors.phone}">
+                <transition name="fade-form">
+                  <p class="form-error" v-if="errors.phone">{{errors.phone}}</p>
+                </transition>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="form-group">
+                <textarea placeholder="Your Message" rows="9" v-model="fields.message" v-bind:class="{'error': errors.message}"></textarea>
+                <transition name="fade-form">
+                  <p class="form-error" v-if="errors.message">{{errors.message}}</p>
+                </transition>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <button type="submit" @click="formValidate" >Send Message</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
