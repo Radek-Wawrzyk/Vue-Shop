dda4af3db1b94a29530465ac83b9f6fb3fc75288
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-25 14:18:46 +0200

    Add - Base markup structure and styling for Cart Dropdown view

--- src/Components/Navigation.vue
+++ src/Components/Navigation.vue
@@ -3,20 +3,23 @@
         <div class="navigation-brand">
 
         </div>
-        <ul v-bind:class="{'toggled': isToggled}" class="navigation-menu">
+        <ul v-bind:class="{'menuToggled': isMenuToggled}" class="navigation-menu">
             <li class="navigation-item"><router-link to="/">Home </router-link></li>
             <li class="navigation-item"><router-link to="/shop">Shop </router-link></li>
             <li class="navigation-item"><router-link to="/about">About </router-link></li>
             <li class="navigation-item"><router-link to="/contact">Contact </router-link></li>
             <li class="navigation-item mobile-visible"><router-link to="/cart">Cart </router-link></li>
         </ul>
-        <button v-on:click="toggle" class="navigation-hamburger">
+        <button v-on:click="toggleMenu" class="navigation-hamburger">
             <span class="hamburger-segment"/>
         </button>
-        <button class="navigation-cart">
+        <button v-on:click="toggleCart" class="navigation-cart">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                <path stroke="black" d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z" />
+                <path fill="gray" d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z" />
             </svg>
+            <ul v-bind:class="{'cartToggled': isCartToggled}" class="cart-shopping">
+              
+            </ul>
         </button>
     </nav>
 </template>
@@ -30,22 +33,28 @@
         {
             data() {
                 return {
-                    isToggled: false,
+                    isMenuToggled: false,
+                    isCartToggled: false,
                     body: document.querySelector('body')
                 };
             },
             methods:
             {
-                toggle: function ()
+                toggleMenu: function ()
                 {
-                    this.isToggled = !this.isToggled;
+                    this.isMenuToggled = !this.isMenuToggled;
 
-                    if (this.isToggled) {
+                    if (this.isMenuToggled) {
                         this.body.className = 'body-navbar-hidden';
                     }
                     else {
                         this.body.className = '';
                     }
+                },
+
+                toggleCart: function ()
+                {
+                    this.isCartToggled = !this.isCartToggled;
                 }
             }
         }
