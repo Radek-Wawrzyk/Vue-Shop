316f7c75b8071e183117a00cb0bb82061ae90a6e
Radek <radek511@op.pl>
2018-06-28 19:29:43 +0200

    Add - Filter component structure with styles

---
+++ src/Components/Shop/ShopFilters.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="filters">
+    <div class="filter-group">
+      <h3 class="filter-title">Search</h3>
+      <div class="search-input">
+        <input type="text" class="search" placeholder="Product name">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
+      </div>
+    </div>
+   <div class="filter-group">
+     <h3 class="filter-title">Strap type</h3>
+     <ul class="filter-options">
+       <li>
+          <input class="checkbox" id="checkbox-1" type="checkbox">
+          <label for="checkbox-1">Leather</label>
+       </li>
+       <li>
+          <input class="checkbox" id="checkbox-2" type="checkbox">
+          <label for="checkbox-2">Metal</label>
+       </li>
+       <li>
+          <input class="checkbox" id="checkbox-3" type="checkbox">
+          <label for="checkbox-3">Rubber</label>
+       </li>
+     </ul>
+   </div>
+   <div class="filter-group">
+     <h3 class="filter-title">Watch shape</h3>
+     <ul class="filter-options">
+       <li>
+          <input class="checkbox" id="checkbox-4" type="checkbox" >
+          <label for="checkbox-4">Circle</label>
+       </li>
+       <li>
+          <input class="checkbox" id="checkbox-5" type="checkbox">
+          <label for="checkbox-5">Rectangle</label>
+       </li>
+       <li>
+          <input class="checkbox" id="checkbox-6" type="checkbox">
+          <label for="checkbox-6">Other</label>
+       </li>
+     </ul>
+   </div>
+   <div class="filter-group">
+     <h3 class="filter-title">Style</h3>
+     <ul class="filter-options">
+       <li>
+          <input class="checkbox" id="checkbox-7" type="checkbox">
+          <label for="checkbox-7">Elegant</label>
+       </li>
+       <li>
+          <input class="checkbox" id="checkbox-8" type="checkbox">
+          <label for="checkbox-8">Sport</label>
+       </li>
+     </ul>
+   </div>
+   <div class="filter-group">
+     <h3 class="filter-title">Type</h3>
+     <ul class="filter-options">
+       <li>
+          <input class="checkbox" id="checkbox-9" type="checkbox">
+          <label for="checkbox-9">Analogue</label>
+       </li>
+       <li>
+          <input class="checkbox" id="checkbox-10" type="checkbox">
+          <label for="checkbox-10">Digital</label>
+       </li>
+        <li>
+          <input class="checkbox" id="checkbox-11" type="checkbox">
+          <label for="checkbox-11">Combination</label>
+       </li>
+     </ul>
+   </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: "ProductFilters",
+  computed: {
+    products() {
+      return this.$store.products;
+    }
+  }
+}
+
+</script>
