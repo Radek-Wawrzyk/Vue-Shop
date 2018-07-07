29e5d9673243d6ccd58c5354feb8f0f7727e86cf
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-07-07 14:01:30 +0200

    Add - Possibility to move the slider without any limitations

--- src/Components/Shop/ShopItemPage.vue
+++ src/Components/Shop/ShopItemPage.vue
@@ -2,31 +2,39 @@
   <main class="item-page">
     <div class="item-container">
       <div class="item-gallery">
-        <div class="item-image">
-          <img class="item-photo" src="http://placehold.it/400x450"/>
+        <div class="item-frame">
+          <img class="item-image" src="http://placehold.it/400x450"/>
         </div>
         <div class="item-slider">
+          <button type="button" v-on:click="moveBack" class="slider-back">
+            <svg aria-hidden="true" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-chevron-left fa-w-10 fa-3x"><path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path></svg>
+          </button>
           <div class="slider-container">
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
-            <img src="http://placehold.it/80x80" class="photo-indicator"/>
+            <div v-bind:style="{ left: sliderTranslation + 'px' }" class="slider-frame">
+              <img src="https://images.rolex.com/catalogue/images/upright-bba-with-shadow/m116400gv-0002.png" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+              <img src="http://placehold.it/80x80" class="photo-indicator"/>
+            </div>
           </div>
+          <button type="button" v-on:click="moveNext" class="slider-next">
+            <svg aria-hidden="true" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-chevron-right fa-w-10 fa-3x"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg>
+          </button>
         </div>
       </div>
       <div class="item-brief">
@@ -71,12 +79,29 @@
   </main>
 </template>
 
-<style lang="scss">
-  @import "ShopItemPage.scss";
-</style>
+<style lang="scss" src="./ShopItemPage.scss"></style>
 
 <script>
   export default {
     name: "ShopItemPage",
+    data()
+    {
+      return {
+        sliderTranslation: 0,
+        translationStrength: 15
+      }
+    },
+    methods: {
+      moveBack: function () 
+      {
+        this.sliderTranslation -= this.translationStrength;
+        console.log(this.sliderTranslation);
+      },
+      moveNext: function()
+      {
+        this.sliderTranslation += this.translationStrength;
+        console.log(this.sliderTranslation);
+      }
+    }
   }
 </script>
