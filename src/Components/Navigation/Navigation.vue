9b1cd6b4681b7c0441814b22d55fa1b2e34b08ee
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-07-13 14:51:48 +0200

    Fix - Issue with auto toggling mobile menu after selecting a menu item in desktop view

--- src/Components/Navigation/Navigation.vue
+++ src/Components/Navigation/Navigation.vue
@@ -12,16 +12,16 @@
     </div>
     <div class="navigation-center">
       <ul v-bind:class="{'menuToggled': isMenuToggled}" class="navigation-menu">
-        <li v-on:click="toggleMenu" class="navigation-item">
+        <li class="navigation-item">
           <router-link aria-label="Home Page" to="/">Home </router-link>
         </li>
-        <li v-on:click="toggleMenu" class="navigation-item">
+        <li class="navigation-item">
           <router-link aria-label="Shop Page" to="/shop">Shop </router-link>
         </li>
-        <li v-on:click="toggleMenu" class="navigation-item">
+        <li class="navigation-item">
           <router-link arial-label="About us" to="/about">About </router-link>
         </li>
-        <li v-on:click="toggleMenu" class="navigation-item">
+        <li class="navigation-item">
           <router-link aria-label="Contact Us" to="/contact">Contact </router-link>
         </li>
       </ul>
@@ -37,31 +37,30 @@
 </template>
 
 <script>
+import Cart from "./Cart/Cart";
+import Search from "./Search/Search";
+import Settings from "./Settings/Settings";
 
-  import Cart from "./Cart/Cart"
-  import Search from "./Search/Search";
-  import Settings from "./Settings/Settings";
-
-  export default {
-    name: "Navigation",
-    components: {Settings, Search, Cart },
-    data() {
-      return {
-        isMenuToggled: false,
-        body: document.querySelector('body'),
-      };
-    },
-    methods: {
-      toggleMenu: function () {
-        this.isMenuToggled = !this.isMenuToggled;
-        if (this.isMenuToggled) {
-          this.body.className = 'body-navbar-hidden';
-        } else {
-          this.body.className = '';
-        }
+export default {
+  name: "Navigation",
+  components: { Settings, Search, Cart },
+  data() {
+    return {
+      isMenuToggled: false,
+      body: document.querySelector("body")
+    };
+  },
+  methods: {
+    toggleMenu: function() {
+      this.isMenuToggled = !this.isMenuToggled;
+      if (this.isMenuToggled) {
+        this.body.className = "body-navbar-hidden";
+      } else {
+        this.body.className = "";
       }
     }
   }
+};
 </script>
 
 <style lang="scss" src="./Navigation.scss" />
