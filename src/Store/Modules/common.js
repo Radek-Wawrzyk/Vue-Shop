62a003d804ce295aca469d122e89c09805c43dd3
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-07-17 12:26:11 +0200

    Divide -  Store into Modules for Cart, Products and Common things

---
+++ src/Store/Modules/common.js
@@ -0,0 +1,52 @@
+const Common = {
+	state: {
+		activeClasses: {
+			settings: false,
+			cart: false
+		},
+		body: document.querySelector("body")
+	},
+	getters: {
+		toggleCartStatus(state) {
+			return state.activeClasses.cart;
+		},
+		toggleSettingsStatus(state) {
+			return state.activeClasses.settings;
+		}
+	},
+	mutations: {
+		toggleCart(state) {
+			state.activeClasses.cart = !state.activeClasses.cart;
+			state.activeClasses.settings = false;
+
+			if (state.activeClasses.cart) {
+				state.body.className = "overflow-hidden";
+			} else {
+				state.body.className = "";
+			}
+
+		},
+		toggleSettings(state) {
+			state.activeClasses.settings = !state.activeClasses.settings;
+			state.activeClasses.cart = false;
+
+			if (state.activeClasses.settings) {
+				state.body.className = "overflow-hidden";
+			} else {
+				state.body.className = "";
+			}
+		}
+	},
+	actions: {
+		toggleCart(event) {
+			event.commit("toggleCart");
+		},
+		toggleSettings(event) {
+			event.commit("toggleSettings");
+		}
+	}
+}
+
+export default {
+	Common
+}
