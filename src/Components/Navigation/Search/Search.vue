91334147ec06edf55ceac243e4c9246d2ae7ecf9
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-07-18 21:42:21 +0200

    Add - Searchbar to the Navigation

--- src/Components/Navigation/Search/Search.vue
+++ src/Components/Navigation/Search/Search.vue
@@ -1,15 +1,33 @@
 <template>
   <li class="navigation-icons-item">
-    <button class="navigation-icon">
+    <transition name="fade">
+      <div v-show="isSearchBarToggled" class="searchbar-container">
+        <input class="searchbar-text" type="text"/>
+        <button v-on:click="toggleSearchBar" class="searchbar-close" type="button">
+          <svg aria-hidden="true" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+        </button>
+      </div>
+    </transition>
+    <button v-bind:class="{active : isSearchBarToggled}" v-on:click="toggleSearchBar" class="navigation-icon">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" /></svg>
     </button>
   </li>
 </template>
 
 <script>
-  export default {
-    name: "Search"
+export default {
+  name: "Search",
+  data() {
+    return {
+      isSearchBarToggled: false
+    };
+  },
+  methods: {
+    toggleSearchBar: function() {
+      this.isSearchBarToggled = !this.isSearchBarToggled;
+    }
   }
+};
 </script>
 
-<style scoped></style>
+<style lang="scss" src="./Search.scss" ></style>
