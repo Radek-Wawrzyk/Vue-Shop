c7623af6a7fe4ef2ce6e24728da2ea404558b49b
Radek <radek511@op.pl>
2018-07-20 14:49:28 +0200

    Edit - Search component ( accessibility & styles )

--- src/Components/Navigation/Search/Search.vue
+++ src/Components/Navigation/Search/Search.vue
@@ -2,13 +2,15 @@
   <li class="navigation-icons-item">
     <transition name="fade">
       <div @keyup.esc="toggleSearchBar" v-show="isSearchBarToggled" class="searchbar-container">
-        <input v-model="queryString" @change="performQuery" class="searchbar-text" type="text"/>
+        <input v-model="queryString" @change="performQuery" placeholder="Search your product" class="searchbar-text" type="text" aria-label="Search your product"/>
         <button v-on:click="toggleSearchBar" class="searchbar-close" type="button">
-          <svg aria-hidden="true" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+          <svg enable-background="new 0 0 31.112 31.112" version="1.1" viewBox="0 0 31.112 31.112" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+            <polygon points="31.112 1.414 29.698 0 15.556 14.142 1.414 0 0 1.414 14.142 15.556 0 29.698 1.414 31.112 15.556 16.97 29.698 31.112 31.112 29.698 16.97 15.556"/>
+          </svg>
         </button>
       </div>
     </transition>
-    <button v-bind:class="{active : isSearchBarToggled}" v-on:click="toggleSearchBar" class="navigation-icon">
+    <button v-bind:class="{active : isSearchBarToggled}" v-on:click="toggleSearchBar" class="navigation-icon" aria-label="Close search bar">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" /></svg>
     </button>
   </li>
