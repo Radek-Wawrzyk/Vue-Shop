8e3094816457199eb4023089470353281014ae74
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-07-25 12:50:09 +0200

    Add - Vue-Slider-Component to the project and customize it's appearance

--- src/Components/Shop/Shop Filters/ShopFilters.vue
+++ src/Components/Shop/Shop Filters/ShopFilters.vue
@@ -7,7 +7,16 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
       </div>
     </div>
-   <div class="filter-group">
+    <div class="filter-group">
+      <div class="filter-price">
+        <VueSlider v-bind="sliderData" v-model="priceRange"/>
+        <div class="price-ranges">
+          <div class="range-display">{{ minimumRange }}</div>
+          <div class="range-display">{{ maximumRange }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="filter-group">
      <h3 class="filter-title">Strap type</h3>
      <ul class="filter-options">
        <li>
@@ -75,14 +84,31 @@
 </template>
 
 <script>
+import VueSlider from "vue-slider-component";
+
 export default {
   name: "ProductFilters",
+  components: {
+    VueSlider
+  },
   data() {
     return {
       checkedShapes: [],
       checkedStyles: [],
       checkedTypes: [],
-      checkedStraps: []
+      checkedStraps: [],
+      sliderData: {
+        min: 0,
+        max: 220,
+
+        tooltipStyle: {
+          display: "none"
+        },
+        processStyle: {
+          backgroundColor: "#e4c7a2"
+        }
+      },
+      priceRange: [0,220]
     };
   },
   methods: {
@@ -103,8 +129,19 @@ export default {
       this.$store.state.products.checkedTypes = this.checkedTypes;
       this.$store.state.products.checkedShapes = this.checkedShapes;
     }
+  },
+  computed:
+  {
+    minimumRange()
+    {
+      return this.priceRange[0] + '$';
+    },
+    maximumRange()
+    {
+      return this.priceRange[1] + '$';
+    }
   }
 };
 </script>
 
-<styles lang="scss" src="./ShopFilters.scss" scopted />
+<style lang="scss" src="./ShopFilters.scss" scoped />
